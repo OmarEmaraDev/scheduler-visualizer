db806ead42ed9b5c2fc3f73c3822d655694d3301
OmarEmaraDev <mail@OmarEmara.dev>
2020-04-08 18:54:48 +0200

    Correct include paths.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
-#include "imgui/imgui.h"
-
-#include "imgui/examples/imgui_impl_glfw.h"
-#include "imgui/examples/imgui_impl_opengl3.h"
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
