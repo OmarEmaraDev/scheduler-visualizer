45b3003745f523c69c189f2bb2d0a1e630ac3931
OmarEmaraDev <mail@OmarEmara.dev>
2020-04-18 22:20:00 +0200

    Add scheduling routine.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "imgui.h"
 #include "imgui_impl_glfw.h"
@@ -6,6 +7,9 @@
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
+#include <queue>
+#include <vector>
 
 #define MAX_SIZE 32
 
@@ -54,6 +58,63 @@ float computeAverageProcessResponseTime(Process processes[MAX_SIZE],
     averageResponseTime += computeProcessResponseTime(processes + i);
   }
   return averageResponseTime / (float)numberOfProcesses;
+}
+
+static int compareInt(int left, int right) { return left - right; }
+
+static int compareFloat(float left, float right) {
+  if (left == right) {
+    return 0;
+  } else if (left > right) {
+    return 1;
+  } else {
+    return -1;
+  }
+}
+
+static int compareProcessByArrivalTime(const void *left, const void *right) {
+  return compareFloat(((Process *)left)->arrivalTime,
+                      ((Process *)right)->arrivalTime);
+}
+
+static int compareProcessByPID(const void *left, const void *right) {
+  return compareInt(((Process *)left)->PID, ((Process *)right)->PID);
+}
+
+struct PriorityComparator {
+  bool operator()(Process *left, Process *right) {
+    return left->priority > right->priority;
+  }
+};
+
+static void
+scheduleProcesses(Process processes[MAX_SIZE], int numberOfProcesses) {
+  qsort(processes, numberOfProcesses, sizeof(Process),
+        compareProcessByArrivalTime);
+
+  std::priority_queue<Process *, std::vector<Process *>, PriorityComparator>
+      priorityQueue;
+
+  float timeElapsed = 0.0f;
+  int numberOfScheduledProcesses = 0;
+  while (numberOfScheduledProcesses < numberOfProcesses) {
+    do {
+      priorityQueue.push(processes + numberOfScheduledProcesses);
+      timeElapsed = std::max(processes[numberOfScheduledProcesses].arrivalTime,
+                             timeElapsed);
+      numberOfScheduledProcesses++;
+    } while (numberOfScheduledProcesses < numberOfProcesses &&
+             processes[numberOfScheduledProcesses].arrivalTime <= timeElapsed);
+
+    while (!priorityQueue.empty()) {
+      Process *process = priorityQueue.top();
+      process->scheduledTime = timeElapsed;
+      timeElapsed += process->burstTime;
+      priorityQueue.pop();
+    }
+  }
+
+  qsort(processes, numberOfProcesses, sizeof(Process), compareProcessByPID);
 }
 
 int main() {
@@ -134,6 +195,7 @@ int main() {
     if (ImGui::Button("Add Process")) {
       if (numberOfProcesses < MAX_SIZE) {
         Process *process = processes + numberOfProcesses;
+        process->PID = numberOfProcesses;
         process->priority = numberOfProcesses;
         process->arrivalTime = (float)numberOfProcesses;
         process->burstTime = 1.0f;
@@ -155,13 +217,15 @@ int main() {
     ImGui::BeginChild("Scrolling", ImVec2(0, height), true,
                       ImGuiWindowFlags_HorizontalScrollbar);
 
+    scheduleProcesses(processes, numberOfProcesses);
+
     for (int i = 0; i < numberOfProcesses; i++) {
       Process process = processes[i];
       ImGui::PushID(i);
       ImGui::AlignTextToFramePadding();
       ImGui::Text("%i", i);
       ImGui::SameLine();
-      ImGui::Dummy(ImVec2(process.arrivalTime * timeScale, 0));
+      ImGui::Dummy(ImVec2(process.scheduledTime * timeScale, 0));
       ImGui::SameLine();
       ImGui::Button("", ImVec2(process.burstTime * timeScale, 0));
       if (ImGui::IsItemHovered()) {
